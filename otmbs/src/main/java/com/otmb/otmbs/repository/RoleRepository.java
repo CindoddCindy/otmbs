a1f845aa0bcaaba391ed0bde31ceab50144f6099
CindoddCindy <cindodcindy@gmail.com>
2021-03-12 16:20:05 +0700

    one to many bidirectional

---
+++ otmbs/src/main/java/com/otmb/otmbs/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.otmb.otmbs.repository;
+
+import com.otmb.otmbs.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository  extends  JpaRepository<Role, Long>{
+    Optional<Role> findByName(String name);
+}
---
+++ otmbs/src/test/java/com/otmb/otmbs/OtmbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otmb.otmbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtmbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
